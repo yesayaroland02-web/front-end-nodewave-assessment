a3be39757b55476513542297847ac9f13bb90b28
yesayaroland02-web <yesayaroland02@gmail.com>
2026-06-06 23:51:10 +0700

    Update task.store.ts

--- store/task.store.ts
+++ store/task.store.ts
@@ -5,25 +5,26 @@ export type TaskStatus = "TODO" | "IN_PROGRESS" | "DONE";
 export type Task = {
   id: string;
   title: string;
-  description: string;
+  description?: string;
   status: TaskStatus;
 };
 
 type TaskStore = {
-  tasks: Task[];
-  setTasks: (tasks: Task[]) => void;
-  updateTask: (task: Task) => void;
+  tasks: {
+    TODO: Task[];
+    IN_PROGRESS: Task[];
+    DONE: Task[];
+  };
+
+  setTasks: (tasks: TaskStore["tasks"]) => void;
 };
 
 export const useTaskStore = create<TaskStore>((set) => ({
-  tasks: [],
+  tasks: {
+    TODO: [],
+    IN_PROGRESS: [],
+    DONE: [],
+  },
 
   setTasks: (tasks) => set({ tasks }),
-
-  updateTask: (updated) =>
-    set((state) => ({
-      tasks: state.tasks.map((t) =>
-        t.id === updated.id ? updated : t
-      ),
-    })),
 }));
